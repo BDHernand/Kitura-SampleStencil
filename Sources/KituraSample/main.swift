45945474b95ae95f3277258c054d1d4a91c8c4aa
Vadim Eisenberg <vadime@il.ibm.com>
2016-08-21 07:43:26 +0300

    updated to the 0503 snapshot syntax (copied from Kitura-Sample)

--- Sources/KituraSample/main.swift
+++ Sources/KituraSample/main.swift
@@ -16,9 +16,12 @@
 
 // KituraSample shows examples for creating custom routes.
 
+import Foundation
+
 import KituraSys
 import KituraNet
-import KituraRouter
+import Kitura
+import KituraStencil
 
 import LoggerAPI
 import HeliumLogger
@@ -27,11 +30,6 @@ import HeliumLogger
     import Glibc
 #endif
 
-import Foundation
-
-import KituraStencil
-
-
 // All Web apps need a router to define routes
 let router = Router()
 
@@ -39,73 +37,74 @@ let router = Router()
 Log.logger = HeliumLogger()
 
 /**
-* RouterMiddleware can be used for intercepting requests and handling custom behavior
-* such as authentication and other routing
-*/
+ * RouterMiddleware can be used for intercepting requests and handling custom behavior
+ * such as authentication and other routing
+ */
 class BasicAuthMiddleware: RouterMiddleware {
     func handle(request: RouterRequest, response: RouterResponse, next: () -> Void) {
-
         let authString = request.headers["Authorization"]
-
         Log.info("Authorization: \(authString)")
-
-        next()
         // Check authorization string in database to approve the request if fail
         // response.error = NSError(domain: "AuthFailure", code: 1, userInfo: [:])
-       
         next()
     }
 }
 
+// Variable to post/put data to (just for sample purposes)
+var name: String?
 
 // This route executes the echo middleware
-router.use("/*", middleware: BasicAuthMiddleware())
+router.all(middleware: BasicAuthMiddleware())
 
-router.use("/static/*", middleware: StaticFileServer())
+router.all("/static", middleware: StaticFileServer())
 
 router.get("/hello") { _, response, next in
-     response.setHeader("Content-Type", value: "text/plain; charset=utf-8")
-     do {
-         try response.status(HttpStatusCode.OK).send("Hello World, from Kitura!").end()
-     }
-     catch {}
-     next()
+    response.headers["Content-Type"] = "text/plain; charset=utf-8"
+    do {
+        let fName = name ?? "World"
+        try response.status(.OK).send("Hello \(fName), from Kitura!").end()
+    } catch {
+        Log.error("Failed to send response \(error)")
+    }
 }
 
 // This route accepts POST requests
 router.post("/hello") {request, response, next in
-    response.setHeader("Content-Type", value: "text/plain; charset=utf-8")
+    response.headers["Content-Type"] = "text/plain; charset=utf-8"
     do {
-        try response.status(HttpStatusCode.OK).send("Got a POST request").end()
+        name = try request.readString()
+        try response.status(.OK).send("Got a POST request").end()
+    } catch {
+        Log.error("Failed to send response \(error)")
     }
-    catch {}
-    next()
 }
 
 // This route accepts PUT requests
 router.put("/hello") {request, response, next in
-    response.setHeader("Content-Type", value: "text/plain; charset=utf-8")
+    response.headers["Content-Type"] = "text/plain; charset=utf-8"
     do {
-        try response.status(HttpStatusCode.OK).send("Got a PUT request").end()
+        name = try request.readString()
+        try response.status(.OK).send("Got a PUT request").end()
+    } catch {
+        Log.error("Failed to send response \(error)")
     }
-    catch {}
-    next()
 }
 
 // This route accepts DELETE requests
 router.delete("/hello") {request, response, next in
-    response.setHeader("Content-Type", value: "text/plain; charset=utf-8")
+    response.headers["Content-Type"] = "text/plain; charset=utf-8"
     do {
-        try response.status(HttpStatusCode.OK).send("Got a DELETE request").end()
+        name = nil
+        try response.status(.OK).send("Got a DELETE request").end()
+    } catch {
+        Log.error("Failed to send response \(error)")
     }
-    catch {}
-    next()
 }
 
 // Error handling example
 router.get("/error") { _, response, next in
     Log.error("Example of error being set")
-    response.status(HttpStatusCode.INTERNAL_SERVER_ERROR)
+    response.status(.internalServerError)
     response.error = NSError(domain: "RouterTestDomain", code: 1, userInfo: [:])
     next()
 }
@@ -114,29 +113,59 @@ router.get("/error") { _, response, next in
 router.get("/redir") { _, response, next in
     do {
         try response.redirect("http://www.ibm.com")
+    } catch {
+        Log.error("Failed to redirect \(error)")
     }
-    catch {}
-
     next()
 }
 
 // Reading parameters
 // Accepts user as a parameter
 router.get("/users/:user") { request, response, next in
-    response.setHeader("Content-Type", value: "text/html; charset=utf-8")
+    response.headers["Content-Type"] = "text/plain; charset=utf-8"
     let p1 = request.params["user"] ?? "(nil)"
     do {
-        try response.status(HttpStatusCode.OK).send(
+        try response.status(.OK).send(
             "<!DOCTYPE html><html><body>" +
-            "<b>User:</b> \(p1)" +
+                "<b>User:</b> \(p1)" +
             "</body></html>\n\n").end()
+    } catch {
+        Log.error("Failed to send response \(error)")
     }
-    catch {}
+}
+
+// Uses multiple handler blocks
+router.get("/multi", handler: { request, response, next in
+    response.status(.OK).send("I'm here!\n")
+    next()
+    }, { request, response, next in
+        response.send("Me too!\n")
+        next()
+})
+
+router.get("/multi") { request, response, next in
+    response.status(.OK).send("I come afterward..\n")
     next()
 }
 
-router.setTemplateEngine(StencilTemplateEngine())
+// Handles any errors that get set
+router.error { request, response, next in
+    response.headers["Content-Type"] = "text/plain; charset=utf-8"
+    do {
+        let errorDescription: String
+        if let error = response.error {
+            errorDescription = "\(error)"
+        } else {
+            errorDescription = "Unknown error"
+        }
+        try response.send("Caught the error: \(errorDescription)").end()
+    }
+    catch {
+        Log.error("Failed to send response \(error)")
+    }
+}
 
+router.setDefaultTemplateEngine(templateEngine: StencilTemplateEngine())
 router.get("/document") { _, response, next in
     defer {
         next()
@@ -156,33 +185,22 @@ router.get("/document") { _, response, next in
     }
 }
 
-// Handles any errors that get set
-router.error { request, response, next in
-  response.setHeader("Content-Type", value: "text/plain; charset=utf-8")
-    do {
-        try response.send("Caught the error: \(response.error!.localizedDescription)").end()
-    }
-    catch {}
-  next()
-}
-
 // A custom Not found handler
 router.all { request, response, next in
-    if  response.getStatusCode() == .NOT_FOUND  {
+    if  response.statusCode == .notFound  {
         // Remove this wrapping if statement, if you want to handle requests to / as well
         if  request.originalUrl != "/"  &&  request.originalUrl != ""  {
             do {
                 try response.send("Route not found in Sample application!").end()
             }
-            catch {}
+            catch {
+                Log.error("Failed to send response \(error)")
+            }
         }
     }
-
     next()
 }
 
 // Listen on port 8090
-let server = HttpServer.listen(8090,
-    delegate: router)
-
+let server = HTTPServer.listen(port: 8090, delegate: router)
 Server.run()
