bc68915bccd4e87daddb274db9238c38b36cb8d8
Vadim Eisenberg <vadime@il.ibm.com>
2016-09-22 07:41:00 +0300

    IBM-Swift/Kitura#650 updated the dependency version of Kitura-StencilTemplateEngine

--- Package.swift
+++ Package.swift
@@ -27,6 +27,6 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 32),
         .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 0, minor: 17),
-        .Package(url: "https://github.com/IBM-Swift/Kitura-StencilTemplateEngine.git",majorVersion: 0, minor: 32),
+        .Package(url: "https://github.com/IBM-Swift/Kitura-StencilTemplateEngine.git",majorVersion: 0, minor: 33),
     ],
     exclude: ["Makefile", "Package-Builder"])
